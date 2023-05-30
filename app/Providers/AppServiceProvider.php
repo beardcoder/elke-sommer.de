aa0c1cebf3e311ebe29fa0cafbe16b2eb83dd742
Markus Sommer <markussom@gmail.com>
2023-05-30 19:31:49 +0200

    chore: init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use A17\Twill\Facades\TwillAppSettings;
+use A17\Twill\Services\Settings\SettingsGroup;
+use Illuminate\Support\ServiceProvider;
+use A17\Twill\Facades\TwillNavigation;
+use A17\Twill\View\Components\Navigation\NavigationLink;
+
+class AppServiceProvider extends ServiceProvider
+{
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
+
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    TwillNavigation::addLink(
+      NavigationLink::make()
+        ->forModule('pages')
+        ->title('Seiten')
+        ->setChildren([
+          NavigationLink::make()
+            ->forModule('menuLinks')
+            ->title('Navigation'),
+        ])
+    );
+    TwillAppSettings::registerSettingsGroup(
+      SettingsGroup::make()
+        ->name('homepage')
+        ->label('Homepage')
+    );
+  }
+}
