0aaac15bf31a90e8193f12bdc3385d5cf410b857
Markus Sommer <markussom@gmail.com>
2023-05-30 21:11:29 +0200

    chore: init

---
+++ .bin/deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Pull the latest version of the app
+git fetch origin main
+
+# Reset to the latest version
+git reset --hard origin/main
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Clear the old view cache
+php artisan view:clear
+
+# Recreate cache
+php artisan optimize
+
+# Compile npm assets
+/home/viking/bin/pnpm install
+
+# Compile npm assets
+/home/viking/bin/pnpm run build
+
+# Run database migrations
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+# generate sitemap
+php artisan sitemap:generate
+
+echo "Deployment finished!"
