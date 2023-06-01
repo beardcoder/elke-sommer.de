210cf73eab3a4fa6c0f5fde7ba0f2c5176cd3197
Markus Sommer <markussom@gmail.com>
2023-06-01 23:02:39 +0200

    fix: correct urls

--- resources/views/site/linktree.blade.php
+++ resources/views/site/linktree.blade.php
@@ -24,15 +24,14 @@
             </svg>
           </a>
           </a>
-          <a rel="noopener noreferrer" href="#" aria-label="Email"
+          <a rel="noopener noreferrer" href="{!! $item->email !!}" aria-label="Email"
             class="rounded-md p-2 dark:text-gray-100 hover:dark:text-violet-400">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="h-6 w-6">
               <path fill="currentColor"
                 d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5l-8-5h16zm0 12H4V8l8 5l8-5v10z" />
             </svg>
-
           </a>
-          <a rel="noopener noreferrer" href="#" aria-label="Whatsapp"
+          <a rel="noopener noreferrer" href="{!! $item->whatsapp !!}" aria-label="Whatsapp"
             class="rounded-md p-2 dark:text-gray-100 hover:dark:text-violet-400">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="h-6 w-6">
               <path fill="currentColor"
