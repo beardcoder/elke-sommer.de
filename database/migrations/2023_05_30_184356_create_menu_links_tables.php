c3c3d862093555cf8a31504376e750343a208e90
Markus Sommer <markussom@gmail.com>
2023-06-03 20:24:11 +0200

    fix: correct table

--- database/migrations/2023_05_30_184356_create_menu_links_tables.php
+++ database/migrations/2023_05_30_184356_create_menu_links_tables.php
@@ -17,10 +17,7 @@ class CreateMenuLinksTables extends Migration
         ->unsigned()
         ->nullable();
 
-      $table
-        ->text('title')
-        ->unsigned()
-        ->nullable();
+      $table->text('title')->nullable();
 
       // add those 2 columns to enable publication timeframe fields (you can use publish_start_date only if you don't need to provide the ability to specify an end date)
       // $table->timestamp('publish_start_date')->nullable();
