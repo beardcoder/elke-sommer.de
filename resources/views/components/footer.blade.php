b3e0bbe863bb37cbc2c7089ab89ec2a2d49911c6
Markus Sommer <markussom@gmail.com>
2023-06-04 22:27:12 +0200

    feat: responsive menu

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,23 +1,25 @@
 <footer class="mt-24 bg-gray-800 px-4 py-8 text-gray-400">
   <div class="container mx-auto flex flex-wrap items-center justify-center space-y-4 sm:justify-between sm:space-y-0">
-    <div class="flex flex-row space-x-4 pr-3 sm:space-x-8">
-      <div class="flex h-12 w-12 flex-shrink-0 items-center justify-center text-white">
+    <div class="flex w-full flex-col items-center space-y-4 pr-3 md:flex-row md:space-y-0">
+      <div class="mr-4 flex h-12 w-12 flex-shrink-0 items-center justify-center text-white">
         <x-sign />
       </div>
+
       <ul class="flex flex-wrap items-center space-x-4 sm:space-x-8">
         @foreach ($links as $link)
           <li class="flex">
             <a href="{{ route('frontend.page', [$link->slug]) }}">{{ $link->title }}</a>
           </li>
+        @endforeach
       </ul>
-      @endforeach
+
+      <ul class="flex flex-wrap space-x-4 sm:space-x-8 md:ml-auto">
+        @foreach ($social_links as $link)
+          <li>
+            <a rel="noopener noreferrer" target="_blank" href="{{ $link->input('url') }}">{{ $link->input('title') }}</a>
+          </li>
+        @endforeach
+      </ul>
     </div>
-    <ul class="flex flex-wrap space-x-4 pl-3 sm:space-x-8">
-      @foreach ($social_links as $link)
-        <li>
-          <a rel="noopener noreferrer" target="_blank" href="{{ $link->input('url') }}">{{ $link->input('title') }}</a>
-        </li>
-      @endforeach
-    </ul>
   </div>
 </footer>
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,14 +1,36 @@
-<ul class="hidden items-stretch space-x-3 md:flex">
-  @foreach ($links as $link)
-    <li class="flex">
-      <a href="{{ route('frontend.page', [$link->getRelated('page')->first()->slug]) }}"
-        class="-mb-1 flex items-center border-b-2 border-black px-4">{{ $link->title }}</a>
-    </li>
-  @endforeach
-</ul>
-
-<button class="flex justify-end p-4 md:hidden">
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-  </svg>
-</button>
+<nav class="ml-auto hidden items-stretch space-x-3 md:flex">
+  <ul class="hidden items-stretch space-x-3 md:flex">
+    @foreach ($links as $link)
+      <li class="flex">
+        <a href="{{ route('frontend.page', [$link->getRelated('page')->first()->slug]) }}"
+          class="-mb-1 flex items-center border-b-2 border-black px-4">{{ $link->title }}</a>
+      </li>
+    @endforeach
+  </ul>
+</nav>
+<div x-data="{ isOpen: false }" @keydown.escape="isOpen = false">
+  <nav x-show="isOpen" x-cloak class="fixed inset-0 bg-white" x-transition:enter="transition ease-out duration-300"
+    x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
+    x-transition:leave="transition ease-in duration-300" x-transition:leave-start="opacity-100 scale-100"
+    x-transition:leave-end="opacity-0 scale-95">
+    <button x-on:click="isOpen=false" class="absolute right-4 top-4 flex justify-end p-4 md:hidden">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+        stroke="currentColor" class="h-6 w-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    </button>
+    <ul class="flex h-full flex-col items-center justify-center space-y-4">
+      @foreach ($links as $link)
+        <li class="flex">
+          <a href="{{ route('frontend.page', [$link->getRelated('page')->first()->slug]) }}"
+            class="text-3xl">{{ $link->title }}</a>
+        </li>
+      @endforeach
+    </ul>
+  </nav>
+  <button x-on:click="isOpen=true" class="flex justify-end p-4 md:hidden">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+    </svg>
+  </button>
+</div>
