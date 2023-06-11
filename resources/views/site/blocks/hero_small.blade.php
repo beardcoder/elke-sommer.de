5d8c66df9c0fbb75808c5a6635f2786d460d95df
Markus Sommer <markussom@gmail.com>
2023-06-11 23:37:02 +0200

    feat: html render in title and text

--- resources/views/site/blocks/hero_small.blade.php
+++ resources/views/site/blocks/hero_small.blade.php
@@ -2,9 +2,14 @@
   <div class="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
     <div class="mr-auto place-self-center lg:col-span-7">
       <h1 class="mb-4 max-w-2xl font-header text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl">
-        {{ $block->input('title') }}</h1>
-      <p class="mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl">
-        {{ $block->input('text') }}</p>
+        {!! $block->input('title') !!}
+      </h1>
+
+      @if ($block->input('text'))
+        <p class="mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl">
+          {!! $block->input('text') !!}
+        </p>
+      @endif
     </div>
     <div class="lg:col-span-5 lg:mt-0 lg:flex">
       {!! TwillImage::make($block, 'cover_small')->crop('default')->width(640)->height(480)->render([
